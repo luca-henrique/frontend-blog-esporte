bec41b947e1d9e320ebf372e7113fff1a8874ee3
Lucas Paes <lucas@Mac-Pro-de-Lucas.local>
2024-12-02 19:56:59 -0300

    feat: eurgh

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,10 @@ module.exports = {
     es2021: true
   },
   extends: [
-    'plugin:react-hooks/recommended',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:react/recommended'
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended'
   ],
   overrides: [
     {
